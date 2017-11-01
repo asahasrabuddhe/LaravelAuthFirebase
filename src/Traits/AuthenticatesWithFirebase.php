a414858c8f702073bf9e4054e46b54a99dc013ba
Ajitem Sahasrbauddhe <ajitem.s@outlook.com>
2017-11-01 11:26:00 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Traits/AuthenticatesWithFirebase.php
+++ src/Traits/AuthenticatesWithFirebase.php
@@ -15,8 +15,8 @@ trait AuthenticatesWithFirebase
 
     public function save(array $options = [])
     {
-        if( !$this->exists ) {
-            $firebase = App::make('laravel_auth_firebase'); 
+        if (!$this->exists) {
+            $firebase = App::make('laravel_auth_firebase');
             $this->userProvider = new FirebaseUserProvider($firebase->getAuthInstance());
 
             $user_info = json_decode($this->user_info);
@@ -25,7 +25,7 @@ trait AuthenticatesWithFirebase
             $user_info->password = Hash::make($user_info->password);
             $this->user_info = json_encode($user_info);
         }
-        
+
         return parent::save();
     }
 }
